3673f96e65cbf90cfd5094ceb8de7e53bc6f52df
Windbladejg7 <juliangarzon867@gmail.com>
2024-06-02 22:01:21 -0500

    Add files via upload

---
+++ OOP/Práctica #1/Actividad.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author LAB-102-PC23
+ */
+public class Actividad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ OOP/Práctica #1/Instructor.java
@@ -0,0 +1,88 @@
+package entidades;
+
+import java.util.Scanner;
+public class Instructor extends Persona {
+    private String tituloAcademico;
+    private int aniosExperiencia;
+
+    public Instructor() {
+        super();// llamando al cosntructor por defecto
+        // de la clase padre
+    }
+
+    public Instructor(String tituloAcademico, int aniosExperiencia) {
+        this.tituloAcademico = tituloAcademico;
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    public Instructor(String nombres, String ape, String ced,
+            String email, String titulo, int anios) {
+        super(nombres, ape, ced, email);
+        this.tituloAcademico = titulo;
+        this.aniosExperiencia = anios;
+    }
+
+    public String getTituloAcademico() {
+        return tituloAcademico;
+    }
+
+    public void setTituloAcademico(String tituloAcademico) {
+        this.tituloAcademico = tituloAcademico;//falta verificar
+    }
+
+    public int getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    public void setAniosExperiencia(int aniosExperiencia) {
+        this.aniosExperiencia = aniosExperiencia;//falta verificar
+    }
+
+    //sobreescritura de metodos(modificar un metodo heredado)
+    @Override
+    public String toString() {
+        return "Instructor:"
+                + super.toString()
+                + ", tituloAcademico=" + this.getTituloAcademico() 
+                + ", aniosExperiencia=" + this.getAniosExperiencia();
+    }
+
+    @Override
+    public void mostrarInfo() {
+        System.out.println("**********************");
+        System.out.println("Datos de Instructor");
+        super.mostrarInfo();
+        System.out.println("Titulo Academico: "+this.tituloAcademico);
+        System.out.println("Años de experiencia: " +this.aniosExperiencia);
+   }
+
+    @Override
+    public void ingresarDatos(Scanner scan) {
+        System.out.println("\nIngresando Instructor:");
+        super.ingresarDatos(scan);
+        System.out.print("Titulo académico:");
+        this.setTituloAcademico(scan.nextLine());
+        System.out.print("Años de experiencia:");
+        this.setAniosExperiencia(Integer.parseInt(scan.nextLine()));
+    }
+
+    @Override
+    public void saludar() {
+        System.out.println("Buenos días, "
+                + this.getTituloAcademico() + this.getNombres() + " "
+                + this.getApellidos());
+    }
+    
+    @Override
+    public void realizarActividad(){
+        System.out.println("Enseñar");
+    }
+    
+    // metodo propio de la clase Instructor
+    //(no esta en la clase padre)
+    public void darClases(String tema){
+        System.out.println("Enseñando el tema "+
+                tema);
+    }
+    
+}
